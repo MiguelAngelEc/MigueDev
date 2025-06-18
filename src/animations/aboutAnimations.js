af5e8af642e0d064fc047338cd794581a0e5ab4a
MigueEC <filia.castillo@gmail.com>
2025-06-18 09:13:09 -0500

    Cambios de JS dinamicos

--- src/animations/aboutAnimations.js
+++ src/animations/aboutAnimations.js
@@ -6,6 +6,7 @@ export function initAboutAnimations() {
     const aElement = document.querySelector('.about-text-a')
     const pElement = document.querySelector('.text-p')
     const sectionElement = document.querySelector('section');
+    const containerStudy = document.querySelector('.container-study')
 
     gsap.from(divElement, {
         opacity: 0,
@@ -16,7 +17,7 @@ export function initAboutAnimations() {
         scrollTrigger: {
             trigger: sectionElement,
             start: "+=25",
-            end: "+=950",
+            end: "+=450",
             scrub: true
         }
     })
@@ -29,7 +30,7 @@ export function initAboutAnimations() {
         scrollTrigger: {
             trigger: sectionElement,
             start: "+=25",
-            end: "+=850",
+            end: "+=450",
             scrub: true
         }
     })
@@ -41,8 +42,22 @@ export function initAboutAnimations() {
         scrollTrigger: {
             trigger: sectionElement,
             start: "+=25",
-            end: "+=750",
+            end: "+=450",
             scrub: true
+        }
+    })
+
+    gsap.to(containerStudy, {
+        opacity: 1,
+        x: 40,   
+        duration: 1,
+        ease: 'power3.out',
+        scrollTrigger: {
+            trigger: sectionElement,
+            start: "+=500",
+            end: "+=1500",
+            scrub: true,
+            toggleActions: "play none none none"
         }
     })
 }
