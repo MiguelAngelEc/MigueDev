a639ace5b9003dea6f41162773a1f126975107eb
MigueEC <filia.castillo@gmail.com>
2025-08-15 09:57:39 -0500

    change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: "server",
-  integrations: [tailwind(), vercel()]
+  output: "static",
+  integrations: [tailwind()]
 });
