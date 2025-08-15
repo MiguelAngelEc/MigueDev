ae9064d4d6f19e372ee9691e4b4d3e137703821a
MigueEC <filia.castillo@gmail.com>
2025-08-15 09:36:47 -0500

    Agrago Vercel como Despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  output: "server",
+  integrations: [tailwind(), vercel()]
 });
