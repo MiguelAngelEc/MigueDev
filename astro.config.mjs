0ff646e5c339d98a5f6b5e3bdadbb84132680f5d
MigueEC <filia.castillo@gmail.com>
2025-08-21 16:16:40 -0500

    fix: configure Astro for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   output: 'static',
+  adapter: vercel(),
   integrations: [tailwind()],
   vite: {
     ssr: {
